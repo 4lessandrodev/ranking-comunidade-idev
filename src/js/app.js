d9735f4ad49171798af42dffa7ca10f9344e31fd
Alessandro <alessandroadm@live.com>
2020-08-06 15:40:57 -0300

    initial page

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+//MEMBERS
+const FIRST_PLACE = document.getElementById('first_place');
+const SECOUND_PLACE = document.getElementById('secound_place');
+const THIRD_PLACE = document.getElementById('third_place');
+const TABLE_BODY = document.getElementById('tableBody');
+
+const ordernedList = MEMBERS.sort((a, b) => (b.points - a.points));
+
+FIRST_PLACE.getElementsByTagName("img")[0].src = ordernedList[0].avatar;
+FIRST_PLACE.getElementsByTagName("h2")[0].textContent = ordernedList[0].name;
+FIRST_PLACE.getElementsByTagName("p")[0].textContent = ordernedList[0].points;
+
+SECOUND_PLACE.getElementsByTagName("img")[0].src = ordernedList[1].avatar;
+SECOUND_PLACE.getElementsByTagName("h2")[0].textContent = ordernedList[1].name;
+SECOUND_PLACE.getElementsByTagName("p")[0].textContent = ordernedList[1].points;
+
+THIRD_PLACE.getElementsByTagName("img")[0].src = ordernedList[2].avatar;
+THIRD_PLACE.getElementsByTagName("h2")[0].textContent = ordernedList[2].name;
+THIRD_PLACE.getElementsByTagName("p")[0].textContent = ordernedList[2].points;
+
+let trs = ``;
+for (let index = 3; index < ordernedList.length; index++) {
+    trs +=`<tr>
+    <th scope="row">${index + 1}</th>
+    <td><img class="avatar" src="${ordernedList[index].avatar}" alt="avatar"></td>
+    <td>${ordernedList[index].name}</td>
+    <td>${ordernedList[index].points}</td>
+  </tr>`;
+}
+
+TABLE_BODY.innerHTML = trs;
---
+++ src/js/members.js
@@ -0,0 +1,127 @@
+const MEMBERS = [
+    {
+        id:1,
+        name: 'thassiadevilart',
+        avatar: 'https://avatars1.githubusercontent.com/u/46179885?s=460&u=7e81c98ab9f20cb0e925300f4ca5aa7ac96d469c&v=4',
+        points: 280,
+
+    },
+    {
+        id:2,
+        name: 'Vinícius emidio',
+        avatar: 'https://filtrosevelas.com.br/img/default-user.png',
+        points: 250,
+    },
+    {
+        id:3,
+        name: 'Airtow',
+        avatar: 'https://avatars3.githubusercontent.com/u/67306463?s=460&u=a05a872e2bfe8bd2337a483911768b5ea1ba1194&v=4',
+        points: 240,
+
+    },
+    {
+        id:7,
+        name: 'Alessandrodev',
+        avatar: 'https://avatars1.githubusercontent.com/u/34426407?s=460&u=f254a9a50b99e2a8f63ce58958cb97089032e307&v=4',
+        points: 240,
+
+    },
+    {
+        id:4,
+        name: 'Andre Vieira',
+        avatar: 'https://avatars1.githubusercontent.com/u/56928555?s=460&u=b3796a627072a5edfc931b153db555f752491372&v=4',
+        points: 240,
+
+    },
+    {
+        id:5,
+        name: 'CandyDaniele',
+        avatar: 'https://avatars3.githubusercontent.com/u/60582129?s=100&v=4',
+        points: 360,
+
+    },
+    {
+        id:6,
+        name: 'Cosme',
+        avatar: 'https://avatars1.githubusercontent.com/u/45211236?s=460&u=19fdc5e37716714b4b7136474829d2da0cd8fc45&v=4',
+        points: 127,
+
+    },
+    {
+        id:8,
+        name: 'diogo_costa',
+        avatar: 'https://avatars0.githubusercontent.com/u/62573994?s=460&u=876fa58f6a433c6a3caf2fc253b88b71c2800590&v=4',
+        points: 240,
+
+    },
+    {
+        id:9,
+        name: 'Ediney',
+        avatar: 'https://filtrosevelas.com.br/img/default-user.png',
+        points: 95,
+
+    },
+    {
+        id:10,
+        name: 'Gabriel',
+        avatar: 'https://avatars0.githubusercontent.com/u/31799208?s=100&v=4',
+        points: 240,
+
+    },
+    {
+        id:11,
+        name: 'Lettie',
+        avatar: 'https://avatars2.githubusercontent.com/u/54192824?s=460&u=1acbae58170ff0c5e932030b0a1bf51613ac029d&v=4',
+        points: 240,
+
+    },
+    {
+        id:12,
+        name: 'muriloignaccio',
+        avatar: 'https://avatars3.githubusercontent.com/u/54823475?s=100&v=4',
+        points: 0,
+
+    },
+    {
+        id:13,
+        name: 'Rafael Sanches',
+        avatar: 'https://avatars0.githubusercontent.com/u/40367010?s=460&u=aa720d84c98adb3bb53aa7b4e8de3d35249ac775&v=4',
+        points: 240,
+
+    },
+    {
+        id:14,
+        name: 'Ray_vieira',
+        avatar: 'https://avatars3.githubusercontent.com/u/67801932?s=460&u=75843fc2cade9a980be58dd2cf975efbcf12fffb&v=4',
+        points: 240,
+
+    },
+    {
+        id:15,
+        name: 'ronielpontes',
+        avatar: 'https://avatars2.githubusercontent.com/u/8785783?s=460&v=4',
+        points: 0,
+
+    },
+    {
+        id:16,
+        name: 'SrTonn',
+        avatar: 'https://filtrosevelas.com.br/img/default-user.png',
+        points: 0,
+
+    },
+    {
+        id:17,
+        name: 'Almir Acacio',
+        avatar: 'https://filtrosevelas.com.br/img/default-user.png',
+        points: 0,
+
+    },
+    {
+        id:18,
+        name: 'JamesG',
+        avatar: 'https://avatars3.githubusercontent.com/u/43867047?s=460&u=fe07fbcd20ee505c73edfab1ff4a055a32e8aa24&v=4',
+        points: 240,
+
+    },
+];
