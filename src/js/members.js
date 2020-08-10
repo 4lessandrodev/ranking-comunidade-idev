288c555471d33b13894382d7c3242bbe59449647
Alessandro dev <34426407+ALESSANDROLMENEZES@users.noreply.github.com>
2020-08-10 00:52:26 -0300

    Update members.js

--- src/js/members.js
+++ src/js/members.js
@@ -9,7 +9,7 @@ const MEMBERS = [
     {
         id:2,
         name: 'Vinícius emidio',
-        avatar: 'https://filtrosevelas.com.br/img/default-user.png',
+        avatar: 'https://avatars0.githubusercontent.com/u/42459603?s=460&u=314f993208f6634cda425ae23f1c161fe73fda9f&v=4',
         points: 310,
     },
     {
