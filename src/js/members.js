667ff363406729bc1687061ad91412c0b67ea7cc
Alessandro <alessandroadm@live.com>
2020-08-13 17:12:30 -0300

    update member

--- src/js/members.js
+++ src/js/members.js
@@ -3,54 +3,54 @@ const MEMBERS = [
         id:1,
         name: 'thassiadevilart',
         avatar: 'https://avatars1.githubusercontent.com/u/46179885?s=460&u=7e81c98ab9f20cb0e925300f4ca5aa7ac96d469c&v=4',
-        points: 290,
+        points: 590,
 
     },
     {
         id:2,
         name: 'Vinícius emidio',
         avatar: 'https://avatars0.githubusercontent.com/u/42459603?s=460&u=314f993208f6634cda425ae23f1c161fe73fda9f&v=4',
-        points: 310,
+        points: 610,
     },
     {
         id:3,
         name: 'Airtow',
         avatar: 'https://avatars3.githubusercontent.com/u/67306463?s=460&u=a05a872e2bfe8bd2337a483911768b5ea1ba1194&v=4',
-        points: 300,
+        points: 600,
 
     },
     {
         id:7,
         name: 'Alessandrodev',
         avatar: 'https://avatars1.githubusercontent.com/u/34426407?s=460&u=f254a9a50b99e2a8f63ce58958cb97089032e307&v=4',
-        points: 240,
+        points: 540,
 
     },
     {
         id:4,
         name: 'Andre Vieira',
         avatar: 'https://avatars1.githubusercontent.com/u/56928555?s=460&u=b3796a627072a5edfc931b153db555f752491372&v=4',
-        points: 240,
+        points: 540,
     },
     {
         id:5,
         name: 'CandyDaniele',
         avatar: 'https://avatars3.githubusercontent.com/u/60582129?s=100&v=4',
-        points: 370,
+        points: 670,
 
     },
     {
         id:6,
         name: 'Cosme',
         avatar: 'https://avatars1.githubusercontent.com/u/45211236?s=460&u=19fdc5e37716714b4b7136474829d2da0cd8fc45&v=4',
-        points: 137,
+        points: 437,
 
     },
     {
         id:8,
         name: 'diogo_costa',
         avatar: 'https://avatars0.githubusercontent.com/u/62573994?s=460&u=876fa58f6a433c6a3caf2fc253b88b71c2800590&v=4',
-        points: 250,
+        points: 550,
 
     },
     {
@@ -71,7 +71,7 @@ const MEMBERS = [
         id:11,
         name: 'Lettie',
         avatar: 'https://avatars2.githubusercontent.com/u/54192824?s=460&u=1acbae58170ff0c5e932030b0a1bf51613ac029d&v=4',
-        points: 240,
+        points: 540,
 
     },
     {
@@ -85,21 +85,21 @@ const MEMBERS = [
         id:13,
         name: 'Rafael Sanches',
         avatar: 'https://avatars0.githubusercontent.com/u/40367010?s=460&u=aa720d84c98adb3bb53aa7b4e8de3d35249ac775&v=4',
-        points: 250,
+        points: 550,
 
     },
     {
         id:14,
         name: 'Ray_vieira',
         avatar: 'https://avatars3.githubusercontent.com/u/67801932?s=460&u=75843fc2cade9a980be58dd2cf975efbcf12fffb&v=4',
-        points: 250,
+        points: 550,
 
     },
     {
         id:15,
         name: 'Kinha',
         avatar: 'https://avatars0.githubusercontent.com/u/56558061?s=460&u=a3f1ace0a0eef8b5f89a208a1083a2ced7938136&v=4',
-        points: 0,
+        points: 300,
     },
     {
         id:16,
@@ -119,20 +119,13 @@ const MEMBERS = [
         id:18,
         name: 'JamesG',
         avatar: 'https://avatars3.githubusercontent.com/u/43867047?s=460&u=fe07fbcd20ee505c73edfab1ff4a055a32e8aa24&v=4',
-        points: 240,
-
-    },
-    {
-        id:18,
-        name: 'JamesG',
-        avatar: 'https://avatars3.githubusercontent.com/u/43867047?s=460&u=fe07fbcd20ee505c73edfab1ff4a055a32e8aa24&v=4',
-        points: 240,
+        points: 540,
 
     },
     {
         id:19,
         name: 'acarolinapassos',
         avatar: 'https://avatars1.githubusercontent.com/u/60620882?s=460&v=4',
-        points: 10,
+        points: 300,
     },
 ];
